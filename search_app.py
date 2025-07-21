9926b32e91d3f991fb869675ae7d070ee29a7ec9
Odilbek Tokhirov <142138394+odil-T@users.noreply.github.com>
2025-07-21 21:14:03 +0500

    Add files via upload

---
+++ search_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from bs4 import BeautifulSoup
+
+with open("candidates/odilbek_resume.html", "r", encoding="utf-8") as f:
+    soup = BeautifulSoup(f, "lxml")
+
+name = soup.find("h1", class_="punjPpsdqoPsmHQibNXmdAxUKMhEDMdUIGvI inline t-24 v-align-middle break-words").text
+summary = None
+experience = None
+education = None
+certification = None
+
+sections = soup.find_all("section", class_="artdeco-card pv-profile-card break-words mt2")
+
+for section in sections:
+    if section.find("div", id="about"):
+        inner_div = section.find("div", class_="display-flex full-width")
+        summary = inner_div.find("span", attrs={"aria-hidden": "true"}).text
+
+    elif section.find("div", id="experience"):
+        experiences = section.find_all("li", class_="artdeco-list__item lZKdEkCNDkuIWzkkoVlJKIpUQOwgnKJbmPmYMlExGQ fkQyJrHujYUIKEKjRRBeWPEGFVdNswiiuQ")
+        for exp in experiences:
+            job_title = exp.find("span", attrs={"aria-hidden": "true"}).text
+            company = exp.find("span", class_="t-14 t-normal").span.text
+            other_info = exp.find_all("span", class_="t-14 t-normal t-black--light")
+            job_duration = other_info[0].span.text
+            workplace = other_info[1].span.text
+            experience = job_title + "\n" + company + "\n" + job_duration + "\n" + workplace
+
+    elif section.find("div", id="education"):
+        educations = section.find_all("")
+        
